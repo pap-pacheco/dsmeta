abeffca1cb77357fd3486e4164a502cdcca04aec
pap-pacheco <pap.pacheco@gmail.com>
2022-11-22 18:47:12 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,12 @@
+import NotificatioButton from "./components/NotificationButton";
 
 function App() {
-  return(
-    <h1>Olá mundo</h1>
-  )
+  return (
+    <>
+      <h1>Olá mundo</h1>
+      <NotificatioButton />
+    </>
+  );
 }
 
-export default App
+export default App;
